44951b235cd81b7480646b9318560a4d780319ca
lockb9127 <66513347+lockb9127@users.noreply.github.com>
2021-01-17 23:32:38 +0800

    玩家移動
    寫腳本(玩家移動)

--- 3D_knifeD/Assets/腳本/player.cs
+++ 3D_knifeD/Assets/腳本/player.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("腳色移動速度"),Range(0.1f,10.0f)]
+    public float speed = 0.5f;
+
+    public Rigidbody rig;
+    
+    public void Move()   //腳色移動//
     {
         
+        float ws = Input.GetAxis("Vertical");                                  //浮點數名稱,輸入,拿到前後左右軸,內建前後移動名稱
+        float ad = Input.GetAxis("Horizontal");                                //浮點數名稱,輸入,拿到前後左右軸,內建左右移動名稱
+        Vector3 ww = transform.forward * ws+ transform.right*ad;               //三維向量名稱,轉換,前後乘上,按鍵輸入名稱+左右乘上按鍵輸入名稱
+        rig.MovePosition(transform.position + ww* speed);                      //鋼體名稱,移動位置名稱,轉換物件位置,加上三維向量名稱呈上速度
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        Move();                         //腳色移動//
     }
+
 }
